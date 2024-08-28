9dcfa3f317e4d16de69f70ba45e60b99911c2284
Magnus-Elias.Sletten <magnus-elias.sletten@student.uib.no>
2024-08-28 15:43:19 +0200

    test

--- push_to_github.sh
+++ push_to_github.sh
@@ -13,25 +13,6 @@ git config --global init.defaultBranch main
 git config --global user.name "$GITHUB_USERNAME"
 git config --global user.email "$GITHUB_EMAIL"
 
-# Initialize a new Git repository if it doesn't already exist
-if [ ! -d .git ]; then
-    git init
-    git remote add origin https://$GH_TOKEN@github.com/MagnusSletten/ciCdtest.git
-    git fetch origin main
-    git checkout -b main
-else
-    git checkout main
-fi
-
-# Ensure that the working directory is clean
-git reset --hard
-git clean -fd
-
-# Fetch the latest changes from the remote repository to avoid conflicts
-git fetch origin main
-
-# Merge the latest changes into the current branch
-git merge origin/main --no-edit
 
 # Add changes (here assuming database.txt is the file you're tracking)
 git add database.txt
