0d27ffbf9bafb0c43b03fe296975e735db229d8d
Magnus-Elias.Sletten <magnus-elias.sletten@student.uib.no>
2024-08-29 11:05:22 +0200

    message

--- dockerfile
+++ dockerfile
@@ -1,27 +1,20 @@
-# Start from the Python base image
+
 FROM python:3.8
 
-# Set the working directory in the container
 WORKDIR /CICDTEST
 
-# Copy necessary files to the container
 COPY sort_data.py /CICDTEST/sort_data.py
 COPY data.txt /CICDTEST/data.txt
 COPY database.txt /CICDTEST/database.txt
 COPY push_to_github.sh /CICDTEST/push_to_github.sh
 
-# Install necessary tools
 RUN apt-get update && apt-get install -y git
 
-# Install pytest if needed
 RUN pip install pytest
 
-# Make the script executable
 RUN chmod +x /CICDTEST/push_to_github.sh
 
 RUN echo "GH_TOKEN is set to: $GH_TOKEN"
 
-
-# Run the Python script and then the GitHub push scrip
 CMD ["bash", "-c", "python /CICDTEST/sort_data.py && /CICDTEST/push_to_github.sh"]
 
--- push_to_github.sh
+++ push_to_github.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 # Configuration
-GITHUB_REPO_URL="https://github.com/MagnusSletten/ciCdtest.git"  # Use the correct GitHub repository URL
-GITHUB_USERNAME="MagnusSletten"  # Your GitHub username
-GITHUB_EMAIL="magnus.elias.sletten@gmail.com"  # Your GitHub email
-GH_TOKEN=${GH_TOKEN}  # Use the environment variable for the access token
+GITHUB_REPO_URL="https://github.com/MagnusSletten/ciCdtest.git"  
+GITHUB_USERNAME="MagnusSletten"  
+GITHUB_EMAIL="magnus.elias.sletten@gmail.com"  
+GH_TOKEN=${GH_TOKEN}  
 
-# Set default branch to main for this session
+
 git config --global init.defaultBranch main
 
-# Git configuration
+
 git config --global user.name "$GITHUB_USERNAME"
 git config --global user.email "$GITHUB_EMAIL"
 
-# Set the working directory explicitly
+
 WORKDIR="/CICDTEST"
 REPO_DIR="$WORKDIR/ciCdtest"
 
-# Remove the existing repo directory if it exists to start fresh
+
 if [ -d "$REPO_DIR" ]; then
     rm -rf "$REPO_DIR"
 fi
 
-# Clone the repository
+
 git clone https://$GH_TOKEN@github.com/MagnusSletten/ciCdtest.git "$REPO_DIR"
 
 cd $WORKDIR
